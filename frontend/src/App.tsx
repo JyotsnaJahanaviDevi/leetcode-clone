3909bcb46ac906713ef86700b9c6654f71c2b67c
jyo <21wh1a0494@bvrithyderabad.edu.in>
2024-01-05 21:30:37 +0530

    added basic auth

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import './App.css'
 import { Signin } from './components/Signin';
 import { initializeApp } from "firebase/app";
 import { useEffect } from 'react';
+import { RecoilRoot, useRecoilState, useSetRecoilState } from 'recoil';
+import { userAtom } from './store/atoms/user';
 
 const firebaseConfig = {
     apiKey: "AIzaSyB0-Fg9SE5_8Z5XGJz4M4o4ZzFv_oMs36c",
@@ -19,24 +21,46 @@ export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
 function App() {
+  return <RecoilRoot>
+    <StoreApp />
+  </RecoilRoot>
+  
+}
+
+function StoreApp() {
+  const [user, setUser] = useRecoilState(userAtom);
   useEffect(() => {
     onAuthStateChanged(auth, function(user) {
-      if (user) {
-        console.log('This is the user: ', user)
+      if (user && user.email) {
+        setUser({
+          loading: false,
+          user: {
+            email: user.email
+          }
+        })
       } else {
+        setUser({
+          loading: false,
+        })
         // No user is signed in.
         console.log('There is no logged in user');
       }
     });
   }, [])
+  if(user.loading) {
+    return <div>loading...</div>
+  }
+
+  if (!user) {
+    return <div><Signin /></div>
+  }
   return (
     <>
-      <div>
-        <Signin />
-      </div>
+      You are logged in as {user.user?.email}
      
     </>
   )
+  return
 }
 
 export default App
